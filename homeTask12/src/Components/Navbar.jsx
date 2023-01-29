b8d8cb692ae3ca400f5c4a7d0a1629d2ea0cfc7e
Nastya <shulhaevich@gmail.com>
2023-01-29 19:03:45 +0200

    fixed switch

--- homeTask12/src/Components/Navbar.jsx
+++ homeTask12/src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { useState } from "react";
+import {ColorModeContext} from './Theme';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -6,7 +8,6 @@ import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import HomeIcon from "@mui/icons-material/Home";
 import ContactsIcon from "@mui/icons-material/Contacts";
-import logoImg from "../media/logoSW.png";
 import { Container } from "@mui/system";
 import {
 Drawer,
@@ -20,141 +21,149 @@ FormControlLabel,
 Switch,
 styled,
 } from "@mui/material";
-import { useState } from "react";
+import logoImg from "../media/logoSW.png";
+
 
 export const Navbar = () => {
-const [mobileMenu, setMobileMenu] = useState({
-	left: false,
-});
 
-const toggleDrawer = (anchor, open) => (event) => {
-	if (
-	event.type === "keydown" &&
-	(event.type === "Tab" || event.type === "Shift")
-	) {
-	return;
+	const [mobileMenu, setMobileMenu] = useState({
+		left: false,
+	});
+	const [checkedTheme, setCheckedTheme] = useState(false);
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (
+		event.type === "keydown" &&
+		(event.type === "Tab" || event.type === "Shift")
+		) {
+		return;
+		}
+		setMobileMenu({ ...mobileMenu, [anchor]: open });
+	};
+
+	const colorMode = React.useContext(ColorModeContext);
+
+	const handleChange = () => {
+		colorMode.toggleColorMode();
+		setCheckedTheme(checkedTheme => !checkedTheme)
 	}
 
-	setMobileMenu({ ...mobileMenu, [anchor]: open });
-};
+	const list = (anchor) => (
+		<Box
+		sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+		role="presentation"
+		onClick={toggleDrawer(anchor, false)}
+		onKeyDown={toggleDrawer(anchor, false)}
+		>
+		<List>
+			{["Home", "Features", "Listed", "Contact"].map(
+			(text, index) => (
+				<ListItem key={text} disablePadding>
+				<ListItemButton>
+					<ListItemIcon>
+					{index === 0 && <HomeIcon />}
+					{index === 1 && <FeaturedPlayListIcon />}
+					{index === 2 && <ListAltIcon />}
+					{index === 3 && <ContactsIcon />}
+					</ListItemIcon>
+					<ListItemText primary={text} />
+				</ListItemButton>
+				</ListItem>
+			)
+			)}
+		</List>
+		</Box>
+	);
 
-const list = (anchor) => (
-	<Box
-	sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-	role="presentation"
-	onClick={toggleDrawer(anchor, false)}
-	onKeyDown={toggleDrawer(anchor, false)}
-	>
-	<List>
-		{["Home", "Features", "Listed", "Contact"].map(
-		(text, index) => (
-			<ListItem key={text} disablePadding>
-			<ListItemButton>
-				<ListItemIcon>
-				{index === 0 && <HomeIcon />}
-				{index === 1 && <FeaturedPlayListIcon />}
-				{index === 2 && <ListAltIcon />}
-				{index === 3 && <ContactsIcon />}
-				</ListItemIcon>
-				<ListItemText primary={text} />
-			</ListItemButton>
-			</ListItem>
-		)
-		)}
-	</List>
-	</Box>
-);
+	const NavLink = styled(Typography)(() => ({
+		fontSize: "14px",
+		color: "#ccc",
+		fontWeight: "bold",
+		cursor: "pointer",
+		"&:hover": {
+		color: "#fff",
+		},
+	}));
 
-const NavLink = styled(Typography)(({ theme }) => ({
-	fontSize: "14px",
-	color: "#ccc",
-	fontWeight: "bold",
-	cursor: "pointer",
-	"&:hover": {
-	color: "#fff",
-	},
-}));
-
-const NavbarLinksBox = styled(Box)(({ theme }) => ({
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-	gap: theme.spacing(3),
-	[theme.breakpoints.down("md")]: {
-	display: "none",
-	},
-}));
-
-const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
-	cursor: "pointer",
-	display: "none",
-	marginRight: theme.spacing(2),
-	[theme.breakpoints.down("md")]: {
-	display: "block",
-	},
-}));
-
-const NavbarContainer = styled(Container)(({ theme }) => ({
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "space-between",
-	padding: theme.spacing(5),
-	[theme.breakpoints.down("md")]: {
-	padding: theme.spacing(2),
-	},
-}));
-
-const NavbarLogo = styled("img")(({ theme }) => ({
-	cursor: "pointer",
-	[theme.breakpoints.down("md")]: {
-	display: "none",
-	},
-}));
-
-return (
-	<NavbarContainer>
-	<Box
-		sx={{
+	const NavbarLinksBox = styled(Box)(({ theme }) => ({
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center",
-		gap: "2.5rem",
-		}}
-	>
-		<Box sx={{ display: "flex", alignItems: "center" }}>
-		<CustomMenuIcon onClick={toggleDrawer("left", true)} />
-		<Drawer
-			anchor="left"
-			open={mobileMenu["left"]}
-			onClose={toggleDrawer("left", false)}
+		gap: theme.spacing(3),
+		[theme.breakpoints.down("md")]: {
+		display: "none",
+		},
+	}));
+
+	const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
+		cursor: "pointer",
+		display: "none",
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.down("md")]: {
+		display: "block",
+		},
+	}));
+
+	const NavbarContainer = styled(Container)(({ theme }) => ({
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-between",
+		padding: theme.spacing(5),
+		[theme.breakpoints.down("md")]: {
+		padding: theme.spacing(2),
+		},
+	}));
+
+	const NavbarLogo = styled("img")(({ theme }) => ({
+		cursor: "pointer",
+		[theme.breakpoints.down("md")]: {
+		display: "none",
+		},
+	}));
+
+	return (
+		<NavbarContainer>
+		<Box
+			sx={{
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+			gap: "2.5rem",
+			}}
 		>
-			{list("left")}
-		</Drawer>
-		<NavbarLogo sx={{width: 120}} src={logoImg} alt="logo" />
+			<Box sx={{ display: "flex", alignItems: "center" }}>
+			<CustomMenuIcon onClick={toggleDrawer("left", true)} />
+			<Drawer
+				anchor="left"
+				open={mobileMenu["left"]}
+				onClose={toggleDrawer("left", false)}
+			>
+				{list("left")}
+			</Drawer>
+			<NavbarLogo sx={{width: 120}} src={logoImg} alt="logo" />
+			</Box>
+
+			<NavbarLinksBox>
+			<NavLink variant="body2">Home</NavLink>
+			<NavLink variant="body2">Features</NavLink>
+			<NavLink variant="body2">Listed</NavLink>
+			<NavLink variant="body2">Contact</NavLink>
+			</NavbarLinksBox>
 		</Box>
 
-		<NavbarLinksBox>
-		<NavLink variant="body2">Home</NavLink>
-		<NavLink variant="body2">Features</NavLink>
-		<NavLink variant="body2">Listed</NavLink>
-		<NavLink variant="body2">Contact</NavLink>
-		</NavbarLinksBox>
-	</Box>
-
-	<Box
-		sx={{
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-		gap: "1rem",
-		}}
-	>
-		<FormGroup>
-			<FormControlLabel control={<Switch defaultUnChecked/>} sx={{color: "#ccc"}} label="Switch Theme" />
-		</FormGroup>
-	</Box>
-	</NavbarContainer>
-);
+		<Box
+			sx={{
+			display: "flex",
+			alignItems: "center",
+			justifyContent: "center",
+			gap: "1rem",
+			}}>
+			<FormGroup>
+				<FormControlLabel  control={<Switch onChange={handleChange} checked={checkedTheme}/>} sx={{color: "#ccc"}} label="Switch Theme" />
+			</FormGroup>
+		</Box>
+		</NavbarContainer>
+	);
 };
 
 export default Navbar;
