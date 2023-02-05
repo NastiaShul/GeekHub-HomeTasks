f1295767a3349a4b78c33edd785d157bb0cf1372
Anastasiia Shulhaievych <shulhaevich@gmail.com>
2023-02-05 11:35:09 +0200

    Delete homeTaks13 directory

--- homeTaks13/src/components/Preloader/Preloader.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Preloader = () => {
-   return <p>Loading ...</p>;
-};
-
-export default Preloader;
--- homeTaks13/src/helpers/dateConverter.js
+++
@@ -1,6 +0,0 @@
-const dateConverter = (time) => {
-   const newDate = new Date(time * 1000);
-   return newDate.toLocaleString();
-};
-
-export default dateConverter;
